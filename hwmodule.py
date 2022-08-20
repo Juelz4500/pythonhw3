e9a6c482ff03e3e264e88bee34f75b40403e438c
Your Name <Jgarner65@gmail.com>
2022-08-20 15:09:55 -0400

    final changess

---
+++ hwmodule.py
@@ -0,0 +1,19 @@
+
+
+
+def square_feet():
+    w = 200
+    l = 100
+    area = int(w * l)
+    print(area)
+
+square_feet()
+
+
+def circle():
+    pi = 3.14
+    r = 5
+    circumference = float(2 * pi * r)
+    print(circumference)
+
+circle()
